6c3d79f3ca7e650ec0ed08c87c528baedb8d3099
Fabian Jütte <fabian.juette@tu-clausthal.de>
2017-02-01 07:34:59 +0100

    Code formatting

--- Transformation.cs
+++ Transformation.cs
@@ -8,32 +8,29 @@ namespace WpfApp
     {
         public DrawingVisual Affine(ImageSource image, Point s1, Point s2, Point s3, Point d1, Point d2, Point d3)
         {
-            TransformGroup transform = new TransformGroup();
-            //Point s1 = new Point(51, 28);
-            //Point s2 = new Point(177, 28);
-            //Point s3 = new Point(111, 7);
+            TransformGroup transformGroup = new TransformGroup();
+            
+            Vector v1 = s2 - s1;
+            Vector v2 = d2 - d1;
+            Vector v3 = s3 - s1;
+            Vector v4 = d3 - d1;
 
-            //Point d1 = new Point(335, 118);
-            //Point d2 = new Point(395, 60);
-            //Point d3 = new Point(345, 75);
+            var vc1 = Vector.CrossProduct(v3, v1) / v1.Length;
+            var vc2 = Vector.CrossProduct(v4, v2) / v2.Length;
 
-            Vector vs21 = s2 - s1;
-            Vector vd21 = d2 - d1;
-            Vector vs31 = s3 - s1;
-            Vector vd31 = d3 - d1;
-
-            double y1 = Vector.CrossProduct(vs31, vs21) / vs21.Length;
-            double y2 = Vector.CrossProduct(vd31, vd21) / vd21.Length;
-
-            transform.Children.Add(new TranslateTransform(-s1.X, -s1.Y));
-            transform.Children.Add(new ScaleTransform(vd21.Length / vs21.Length, y2 / y1));
-            transform.Children.Add(new RotateTransform(Vector.AngleBetween(vs21, vd21)));
-            transform.Children.Add(new TranslateTransform(d1.X, d1.Y));
+            transformGroup.Children.Add(
+                new TranslateTransform(-s1.X, -s1.Y));
+            transformGroup.Children.Add(
+                new ScaleTransform(v2.Length / v1.Length, vc2 / vc1));
+            transformGroup.Children.Add(
+                new RotateTransform(Vector.AngleBetween(v1, v2)));
+            transformGroup.Children.Add(
+                new TranslateTransform(d1.X, d1.Y));
 
             DrawingVisual vis = new DrawingVisual();
 
             DrawingContext dc = vis.RenderOpen();
-            dc.PushTransform(transform);
+            dc.PushTransform(transformGroup);
             dc.DrawImage(image, new Rect(0, 0, image.Width, image.Height));
             dc.Pop();
             dc.Close();
